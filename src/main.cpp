72241a9d7faad42c11e4a1fe6294bab5663ed2b3
Fangjun Zhou <fzhou48@wisc.edu>
2023-01-05 22:36:18 -0800

    Update: main.cpp Moved the WindowManager to the hpaslt scope.

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,14 @@ int main(int argc, char const* argv[]) {
 
   hpaslt::logger->coreLogger->info("Creating WindowManager.");
   // Create the window manager, start the UI thread.
-  std::shared_ptr<hpaslt::WindowManager> windowMgr;
   try {
-    windowMgr = std::make_shared<hpaslt::WindowManager>();
+    hpaslt::windowMgr = std::make_shared<hpaslt::WindowManager>();
   } catch (const std::system_error& e) {
     return EXIT_FAILURE;
   }
 
   // Join UI thread, get the execution result.
-  int exitRes = windowMgr->execute();
+  int exitRes = hpaslt::windowMgr->execute();
 
   return exitRes;
 }
