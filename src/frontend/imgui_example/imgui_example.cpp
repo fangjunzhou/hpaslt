a5ed251bed7f8cf6e6b79d2cd77a236d2f0c03b7
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-01-11 19:06:14 -0600

    Update: ImGuiExample Added command to set window enable/disable.

--- src/frontend/imgui_example/imgui_example.cpp
+++ src/frontend/imgui_example/imgui_example.cpp
@@ -3,6 +3,7 @@
 #include <imgui_impl_opengl3.h>
 
 #include "imgui_example.h"
+#include "commands/commands.h"
 
 namespace hpaslt {
 
@@ -14,9 +15,20 @@ ImGuiExample::ImGuiExample() : ImGuiObject("ImGui Example") {
   // Listen to the change event.
   m_enableCallbackHandle =
       s_onEnable.append([this](bool enabled) { this->setEnabled(enabled); });
+
+  // Register open/close example window command.
+  m_setConsoleHandle =
+      Commands::registerOnSetConsoleSystem([](csys::System* system) {
+        system->RegisterCommand(
+            "setImGuiExample", "Enable or disable ImGui Example window.",
+            [](bool enable) { s_onEnable(enable); }, csys::Arg<bool>("enable"));
+      });
 }
 
-ImGuiExample::~ImGuiExample() { s_onEnable.remove(m_enableCallbackHandle); }
+ImGuiExample::~ImGuiExample() {
+  s_onEnable.remove(m_enableCallbackHandle);
+  Commands::unregisterOnSetConsoleSystem(m_setConsoleHandle);
+}
 
 void ImGuiExample::render() {
   // Show demo window.
--- src/frontend/imgui_example/imgui_example.h
+++ src/frontend/imgui_example/imgui_example.h
@@ -1,12 +1,18 @@
 #pragma once
 
 #include <eventpp/callbacklist.h>
+#include <csys/csys.h>
 
 #include "window_manager/imgui_object.h"
 
 namespace hpaslt {
 
 class ImGuiExample : public ImGuiObject {
+ private:
+  using SetConsoleCallback = eventpp::CallbackList<void(csys::System*)>;
+
+  SetConsoleCallback::Handle m_setConsoleHandle;
+
  public:
   /**
    * @brief callback event when open the window from other place.
