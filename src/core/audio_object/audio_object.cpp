0b43484c20f76fb4b15daee5f3388643faa1b7d7
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-01-12 23:50:20 -0600

    Update: AudioObject Added mutex lock to guard some AudioObject methods.

--- src/core/audio_object/audio_object.cpp
+++ src/core/audio_object/audio_object.cpp
@@ -3,14 +3,24 @@
 namespace hpaslt {
 
 void AudioObject::loadAudioFile(const std::string& filePath) {
+  // Guard the audio file.
+  std::lock_guard<std::mutex> lock(m_mutex);
+
   if (!m_audioFile.load(filePath)) {
     throw std::invalid_argument("Audio file at path cannot be loaded.");
   }
+
+  resetCursor();
 }
 
 double AudioObject::getLength() {
+  // Guard the audio file.
+  m_mutex.lock();
+
   int numSamples = m_audioFile.getNumSamplesPerChannel();
   int sampleRate = m_audioFile.getSampleRate();
+
+  m_mutex.unlock();
 
   return (double)numSamples / (double)sampleRate;
 }
--- src/core/audio_object/audio_object.h
+++ src/core/audio_object/audio_object.h
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <string>
+#include <mutex>
 
 #include <AudioFile.h>
 
 namespace hpaslt {
+
 class AudioObject {
+  /**
+   * @brief The lock to protect audio object.
+   * Need to acquire the lock to access the audio object.
+   *
+   */
+  std::mutex m_mutex;
+
  private:
   /**
    * @brief The current processing audio file.
@@ -13,9 +22,16 @@ class AudioObject {
    */
   AudioFile<double> m_audioFile;
 
+  /**
+   * @brief Current playing frame.
+   *
+   */
+  int m_cursor;
+
  public:
   /**
    * @brief Load the audio file from path.
+   * This method is thread safe.
    *
    * @param filePath
    */
@@ -29,11 +45,34 @@ class AudioObject {
   AudioFile<double>& getAudioFile() { return m_audioFile; }
 
   /**
+   * @brief Get the mutex lock by reference.
+   *
+   * @return std::mutex&
+   */
+  std::mutex& getMutex() { return m_mutex; }
+
+  /**
    * @brief Get the AudioFile length in seconds.
+   * This method is thread safe.
    *
    * @return double
    */
   double getLength();
+
+  /**
+   * @brief Get the current playing frame.
+   * This method is not thread safe.
+   *
+   * @return int
+   */
+  int getCursor() { return m_cursor; }
+
+  /**
+   * @brief Reset the playing cursor to start.
+   * This method is not thread safe.
+   *
+   */
+  void resetCursor() { m_cursor = 0; }
 };
 
 }  // namespace hpaslt
