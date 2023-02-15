c702fa7acb0a4f594e52f553a80e39b78d70e01c
Fangjun Zhou <fzhou48@wisc.edu>
2023-02-15 15:29:43 -0600

    Added: SignalGeneratorTest

---
+++ tests/common/signal_generator_test.cpp
@@ -0,0 +1,113 @@
+#include <gtest/gtest.h>
+
+#include <math.h>
+#include <AudioFile.h>
+
+#include "common/signal_generator/signal_generator.h"
+
+namespace hpaslt {
+
+namespace test {
+
+class SignalGeneratorTest : public ::testing::Test {
+ protected:
+  /**
+   * @brief The audio file object used by all tests.
+   *
+   */
+  std::shared_ptr<AudioFile<float>> m_audioFile;
+
+  /**
+   * @brief The signal generator used by all tests.
+   *
+   */
+  std::shared_ptr<hpaslt::SignalGenerator> m_signalGenerator;
+
+  SignalGeneratorTest() {}
+  ~SignalGeneratorTest() override {}
+
+  void SetUp() override {
+    // Setup audio.
+    m_audioFile = std::make_shared<AudioFile<float>>();
+    m_audioFile->setNumChannels(2);
+
+    // Setup signal generator.
+    m_signalGenerator = std::make_shared<hpaslt::SignalGenerator>();
+    m_signalGenerator->bindAudioFile(m_audioFile);
+  }
+
+  virtual void TearDown() override {
+    m_audioFile = nullptr;
+    m_signalGenerator = nullptr;
+  }
+
+  /**
+   * @brief Check if the audio file signal is the combination of a list of
+   * frequency.
+   *
+   * @param freq the frequency list.
+   * @return testing::AssertionResult
+   */
+  testing::AssertionResult isFrequency(std::vector<float>& freq,
+                                       float error = 0) {
+    int sampleRate = m_audioFile->getSampleRate();
+    for (int i = 0; i < m_audioFile->getNumSamplesPerChannel(); i++) {
+      // Merge all the frequency together.
+      float expectVal = 0;
+      for (int j = 0; j < freq.size(); j++) {
+        expectVal += sin(2 * M_PI * i * freq[j] / sampleRate);
+      }
+
+      for (int channel = 0; channel < m_audioFile->getNumChannels();
+           channel++) {
+        if (abs(m_audioFile->samples[channel][i] - expectVal) > error) {
+          return testing::AssertionFailure()
+                 << "Sample " << i << " at channel " << channel << " should be "
+                 << expectVal << ", but is actually "
+                 << m_audioFile->samples[channel][i];
+        }
+      }
+    }
+
+    return testing::AssertionSuccess();
+  }
+};
+
+#define TEST_AUDIO_LENGTH 4
+TEST_F(SignalGeneratorTest, ChangeLength) {
+  // Check the default audio channel number and sample rate.
+  ASSERT_EQ(m_audioFile->getNumChannels(), 2);
+  ASSERT_EQ(m_audioFile->getSampleRate(), 44100);
+
+  // Check the default audio length is 0.
+  ASSERT_EQ(m_audioFile->getNumSamplesPerChannel(), 0);
+
+  // Change the length of the audio.
+  m_signalGenerator->changeLength(m_audioFile->getSampleRate() *
+                                  TEST_AUDIO_LENGTH);
+
+  // Check the new audio length.
+  EXPECT_EQ(m_audioFile->getNumSamplesPerChannel(), 44100 * TEST_AUDIO_LENGTH);
+  EXPECT_EQ(m_audioFile->getLengthInSeconds(), 4);
+}
+
+TEST_F(SignalGeneratorTest, GenerateSignal) {
+  // Change the length of the audio.
+  m_signalGenerator->changeLength(m_audioFile->getSampleRate() *
+                                  TEST_AUDIO_LENGTH);
+
+  // Check the new audio length.
+  EXPECT_EQ(m_audioFile->getNumSamplesPerChannel(), 44100 * TEST_AUDIO_LENGTH);
+  EXPECT_EQ(m_audioFile->getLengthInSeconds(), 4);
+
+  // Generate signal.
+  m_signalGenerator->generateSignal(32);
+
+  // Test signal.
+  std::vector<float> freq{32};
+  EXPECT_TRUE(isFrequency(freq));
+}
+
+}  // namespace test
+
+}  // namespace hpaslt
