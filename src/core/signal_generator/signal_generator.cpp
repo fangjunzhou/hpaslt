a3ad66aec59b42c482ea932491e8ad72d1ea57cc
Fangjun Zhou <fzhou48@wisc.edu>
2023-02-16 16:18:10 -0600

    Update: SignalGenerator Support OpenMP.

--- src/core/signal_generator/signal_generator.cpp
+++ src/core/signal_generator/signal_generator.cpp
@@ -11,9 +11,10 @@ void SignalGenerator::changeLength(int length) {
 void SignalGenerator::generateSignal(float freq, float magnitude) {
   int sampleRate = m_workingAudioFile->getSampleRate();
   int sampleLength = m_workingAudioFile->getNumSamplesPerChannel();
-  for (int i = 0; i < sampleLength; i++) {
-    for (int channel = 0; channel < m_workingAudioFile->getNumChannels();
-         channel++) {
+  for (int channel = 0; channel < m_workingAudioFile->getNumChannels();
+       channel++) {
+#pragma omp parallel for
+    for (int i = 0; i < sampleLength; i++) {
       m_workingAudioFile->samples[channel][i] =
           sin(2 * M_PI * i * freq / sampleRate) * magnitude;
     }
@@ -23,9 +24,10 @@ void SignalGenerator::generateSignal(float freq, float magnitude) {
 void SignalGenerator::overlaySignal(float freq, float magnitude) {
   int sampleRate = m_workingAudioFile->getSampleRate();
   int sampleLength = m_workingAudioFile->getNumSamplesPerChannel();
-  for (int i = 0; i < sampleLength; i++) {
-    for (int channel = 0; channel < m_workingAudioFile->getNumChannels();
-         channel++) {
+  for (int channel = 0; channel < m_workingAudioFile->getNumChannels();
+       channel++) {
+#pragma omp parallel for
+    for (int i = 0; i < sampleLength; i++) {
       m_workingAudioFile->samples[channel][i] +=
           sin(2 * M_PI * i * freq / sampleRate) * magnitude;
     }
